6267ee225d5c05f142eb65f313c0ed79d0381f9d
amandamaulida <khumaira.amanda@gmail.com>
2022-06-12 12:22:36 +0700

    challenge promise

--- promise.js
+++ promise.js
@@ -1,7 +1,12 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = new emosi((resolve,reject) => {
+  resolve("marah");
+  reject("tidak marah");
+
+});
+
 
 module.exports = {
   promiseOutput,
